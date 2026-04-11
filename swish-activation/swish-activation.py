9fd0756a53fab2df97991148daaad189f0875b95
Gitaditya Maddali <141127000+gitaditya24@users.noreply.github.com>
2026-04-12 01:53:01 +0530

    Update solution for swish-activation

--- swish-activation/swish-activation.py
+++ swish-activation/swish-activation.py
@@ -6,9 +6,21 @@ def swish(x):
     """
     # Write code here
     def sigma(y):
-        return 1/(1+np.exp(-y))
-    x = np.asarray(x , dtype = float)
-    result = []
-    for val in x:
-        result.append(val*sigma(val))
-    return result
+        return 1 / (1 + np.exp(-y))
+    
+    x = np.asarray(x, dtype=float)
+
+    if x.ndim == 1:
+        result = []
+        for val in x:
+            result.append(val * sigma(val))
+        return result
+    
+    elif x.ndim == 2:
+        result = []
+        for row in x:
+            new_row = []
+            for val in row:
+                new_row.append(val * sigma(val))
+            result.append(new_row)
+        return result
